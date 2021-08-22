b22b5a41e88978909231ca2f2842b67d8f804009
vinsonw <me@vinsonwei.com>
2021-08-23 00:02:50 +0800

    first commit

---
+++ src/com/vinsonw/Main.java
@@ -0,0 +1,15 @@
+package com.vinsonw;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    // variables
+//        int myAge = 30;
+//        int herAge = myAge;
+//        System.out.println(herAge);
+
+        // Primitives
+        // byte, short, int, long, float, double, char, boolean
+    }
+}
