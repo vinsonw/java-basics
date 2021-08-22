2656e68c2594af442d9ae21edf54e939c41b5a8e
vinsonw <me@vinsonwei.com>
2021-08-23 00:34:58 +0800

    spotted first difference between java and python in passing values between variables

--- src/com/vinsonw/Main.java
+++ src/com/vinsonw/Main.java
@@ -1,5 +1,8 @@
 package com.vinsonw;
 
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,7 +12,28 @@ public class Main {
 //        int herAge = myAge;
 //        System.out.println(herAge);
 
-        // Primitives
+        // Primitive Types
         // byte, short, int, long, float, double, char, boolean
+//        byte age = 30;
+//        long viewsCount = 3_123_456_789L; // L is necessary
+//        float price = 10.99F; // F is necessary
+//        char letter = 'A'; // single quotes for letter; double quotes for string.
+//        boolean isEligible = false;
+
+        // Reference Types
+//        Date now = new Date(); // 'now' object is an instance of Date class
+//        now.getTime();
+//        System.out.println(now);
+
+        // different copy behavior between primitive and reference type
+//        byte x = 1;
+//        byte y = x;
+//        x = 2;
+//        System.out.println(y);
+        Point point1 = new Point(1, 1);
+        Point point2 = point1; // point2 and point1 point to the same object
+        point1.x = 2; // use point1 as the handler to update the same object, DIFFER FROM PYTHON.
+        System.out.println(point2);
+
     }
 }
